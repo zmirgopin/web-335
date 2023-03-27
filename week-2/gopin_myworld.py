9e0d7c8817be5be4d8a0d9d1cda0617ef302f136
zmirgopin <zahavagopin@gmail.com>
2023-03-27 15:58:13 -0400

    week 3

---
+++ week-2/gopin_myworld.py
@@ -0,0 +1,8 @@
+#code attribution
+#author: Zahava Gopin
+#title: gopin_myworld.py
+#description: first python document
+#date: 3/22/2023
+
+#printing a message 
+print("You are now in gopin's world!")
