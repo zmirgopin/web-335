3f0548c7195d3757ad143b5a9a8909dcb3f84b9b
zmirgopin <zahavagopin@gmail.com>
2023-04-23 12:02:18 -0400

    week 6

---
+++ week_6/gopin_usersp1.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://web335_user:s3cret@bellevueuniversity.r06aetm.mongodb.net/web335DBretryWrites=true&w=majority")
+
+print(client)
+# Access the desired database and collection
+db= client.web335DB
+collection= db.users
+# Find all documents in the collection
+documents = collection.find()
+print(documents)
+# Find the document with the specified id
+employee= collection.find_one({'employeeId': 1011})
+# Print the document
+print(employee)
+# Find the document with the specified name
+name= collection.find_one({'lastName': 'Mozart'})
+# Print the document
+print(name)
